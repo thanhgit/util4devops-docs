24537de3ff8d3a0b78ccf062e2f05f4ee72b1a5f
Nguyễn Phúc Thạnh <thanh29695@gmail.com>
2024-05-29 12:50:24 +0700

    update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'thanhgit', // Usually your GitHub org/user name.
-  projectName: 'uti4devops', // Usually your repo name.
+  projectName: 'uti4devops-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
